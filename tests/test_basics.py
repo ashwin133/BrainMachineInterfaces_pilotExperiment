2c15b2419865135e2cdb32163b1d24eacfa97c8e
ashwin133 <ashwingunasekaran133@gmail.com>
2024-01-01 15:44:19 +0000

    retry

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -8,10 +8,18 @@ import pynput
 import time
 import multiprocessing
 
-sys.path.insert(0,'/Users/ashwin/Documents/Y4 project Brain Human Interfaces/General 4th year Github repo/PilotExperiment/BrainMachineInterfaces_pilotExperiment')
+import os
 
+# Get the directory of the current file (test_basics.py)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Calculate the relative path to the directory where 'variables' module is located
+variables_dir = os.path.join(current_dir, '../')
+
+# Add this directory to sys.path
+sys.path.insert(0, variables_dir)
 def testGameRuns():
-    sys.path.insert(0,'/Users/ashwin/Documents/Y4 project Brain Human Interfaces/General 4th year Github repo/PilotExperiment/BrainMachineInterfaces_pilotExperiment')
+    
     from variables import initialiseGame
     from setup import runGameSetup
     from gameDynamics import startupGame
@@ -21,4 +29,35 @@ def testGameRuns():
     # next run game setup to load game configurations 
     gameEngine = runGameSetup(gameEngine=gameEngine)
     startupGame(gameEngine=gameEngine)
+
+def testGameStartsAndExitsWithNoError():
+    sys.path.insert(0,'/Users/ashwin/Documents/Y4 project Brain Human Interfaces/General 4th year Github repo/PilotExperiment/BrainMachineInterfaces_pilotExperiment')
+
+
+    with pytest.warns(UserWarning):
+        warnings.warn("DeprecationWarning", UserWarning) 
+        proc1 = multiprocessing.Process(target=runGame, args=())
+        proc2 = multiprocessing.Process(target=endGameByQ, args=())
+        proc1.start()
+        proc2.start()
+        proc1.join()
+        proc2.join()
+        
+
+
+def runGame():
+    from variables import initialiseGame
+    from setup import runGameSetup
+    from gameDynamics import startupGame
+    gameEngine = initialiseGame()
+    # next run game setup to load game configurations 
+    gameEngine = runGameSetup(gameEngine=gameEngine)
+    startupGame(gameEngine=gameEngine)
+
+def endGameByQ():
+    time.sleep(7)
+    from pynput.keyboard import Key, Controller
+    keyboard = Controller()
+    keyboard.press('q')
+    keyboard.release('q')
 
