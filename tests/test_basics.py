0f8c290d723851960e524e0ce3739776b0e555c3
ashwin133 <ashwingunasekaran133@gmail.com>
2024-01-01 15:39:11 +0000

    retry

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -22,34 +22,3 @@ def testGameRuns():
     gameEngine = runGameSetup(gameEngine=gameEngine)
     startupGame(gameEngine=gameEngine)
 
-def testGameStartsAndExitsWithNoError():
-    sys.path.insert(0,'/Users/ashwin/Documents/Y4 project Brain Human Interfaces/General 4th year Github repo/PilotExperiment/BrainMachineInterfaces_pilotExperiment')
-
-
-    with pytest.warns(UserWarning):
-        warnings.warn("DeprecationWarning", UserWarning) 
-        proc1 = multiprocessing.Process(target=runGame, args=())
-        proc2 = multiprocessing.Process(target=endGameByQ, args=())
-        proc1.start()
-        proc2.start()
-        proc1.join()
-        proc2.join()
-        
-
-
-def runGame():
-    from variables import initialiseGame
-    from setup import runGameSetup
-    from gameDynamics import startupGame
-    gameEngine = initialiseGame()
-    # next run game setup to load game configurations 
-    gameEngine = runGameSetup(gameEngine=gameEngine)
-    startupGame(gameEngine=gameEngine)
-
-def endGameByQ():
-    time.sleep(7)
-    from pynput.keyboard import Key, Controller
-    keyboard = Controller()
-    keyboard.press('q')
-    keyboard.release('q')
-
