7a4940d19e66db0bbee8a05ce352dad496fd70e1
ashwin133 <ashwingunasekaran133@gmail.com>
2024-01-01 15:34:55 +0000

    retry

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -10,7 +10,17 @@ import multiprocessing
 
 sys.path.insert(0,'/Users/ashwin/Documents/Y4 project Brain Human Interfaces/General 4th year Github repo/PilotExperiment/BrainMachineInterfaces_pilotExperiment')
 
-
+def testGameRuns():
+    sys.path.insert(0,'/Users/ashwin/Documents/Y4 project Brain Human Interfaces/General 4th year Github repo/PilotExperiment/BrainMachineInterfaces_pilotExperiment')
+    from variables import initialiseGame
+    from setup import runGameSetup
+    from gameDynamics import startupGame
+    gameEngine = initialiseGame()
+    gameEngine.testMode = True
+    gameEngine.testTime = 3000
+    # next run game setup to load game configurations 
+    gameEngine = runGameSetup(gameEngine=gameEngine)
+    startupGame(gameEngine=gameEngine)
 
 def testGameStartsAndExitsWithNoError():
     sys.path.insert(0,'/Users/ashwin/Documents/Y4 project Brain Human Interfaces/General 4th year Github repo/PilotExperiment/BrainMachineInterfaces_pilotExperiment')
@@ -43,17 +53,3 @@ def endGameByQ():
     keyboard.press('q')
     keyboard.release('q')
 
-def testGameRuns():
-    sys.path.insert(0,'/Users/ashwin/Documents/Y4 project Brain Human Interfaces/General 4th year Github repo/PilotExperiment/BrainMachineInterfaces_pilotExperiment')
-    from variables import initialiseGame
-    from setup import runGameSetup
-    from gameDynamics import startupGame
-    gameEngine = initialiseGame()
-    gameEngine.testMode = True
-    gameEngine.testTime = 3000
-    # next run game setup to load game configurations 
-    gameEngine = runGameSetup(gameEngine=gameEngine)
-    startupGame(gameEngine=gameEngine)
-
-if __name__ == "__main__":
-    pass
